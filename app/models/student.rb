2e97abacddbb56f083e863e74093d0122ced9fbf
hossam <hossam@hossams-MacBook-Pro.local>
2023-01-07 22:19:04 +0200

    send email after finish reorder task

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,13 +16,15 @@ class Student < ApplicationRecord
   def self.reorder(school, student)
     byebug
     id = student.id
-    students_to_edit = school.students.where("id > ?" , id)
+    students_to_edit_order = school.students.where("id > ?" , id)
 
-    students_to_edit.each do |student|
+    students_to_edit_order.each do |student|
       old_order = student.order
       new_order = student.order - 1
       student.update(order: new_order)
     end
+
+    TaskConfirmationMailer.task_confirmation_email.deliver_now
   end
   
 end
