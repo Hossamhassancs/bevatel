0af947438d84956e9eb19bd572ffbf9c11cdcc9a
hossam <hossam@hossams-MacBook-Pro.local>
2023-01-07 20:17:22 +0200

    set order by school

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,14 @@ class Student < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :school
+  validates_presence_of :name 
+  after_create :set_order
+
+
+  def set_order
+    school = self.school
+    last_id = school.students.last.order || 0
+    self.update(order: last_id+1)
+  end
   
-  validates_presence_of :name
 end
